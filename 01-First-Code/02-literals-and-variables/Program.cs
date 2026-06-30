5d614c2eba4f49f78a2acc5503decde4512b50f7
anas1306 <mohammed.anas1306@gmail.com>
2026-06-30 20:09:52 -0300

    Completed 02-literals-and-variables

---
+++ 01-First-Code/02-literals-and-variables/Program.cs
@@ -0,0 +1,25 @@
+﻿// Console.WriteLine(true);
+// Console.WriteLine(false);
+
+// string firstName = "Bob";
+// Console.WriteLine(firstName);
+
+// string firstName;
+// firstName = "Bob";
+// Console.WriteLine(firstName);
+// firstName = "Liem";
+// Console.WriteLine(firstName);
+// firstName = "Isabella";
+// Console.WriteLine(firstName);
+// firstName = "Yasmin";
+// Console.WriteLine(firstName);
+
+// var message = "Hello World!";
+// message = 10.703m;
+// couldn't change the value of the variable message since it was assigned as a string first
+
+var name = "Bob";
+var number = 3;
+var deci = 34.3m;
+
+Console.WriteLine($"Hello, {name}! You have {number} messages in your inbox. The temperature is {deci} celsius.");
